a4d6d6f0ed93c27bb0a2df9ef37eaeada870442a
Yauheni Akhotnikau <eao197@gmail.com>
2019-09-06 12:39:40 +0300

    Switch to the fresh versions of dependencies.

--- externals.rb
+++ externals.rb
@@ -1,23 +1,23 @@
 MxxRu::arch_externals :so5 do |e|
-  e.url 'https://bitbucket.org/sobjectizerteam/sobjectizer/downloads/so-5.6.0.1.zip'
+  e.url 'https://github.com/Stiffstream/sobjectizer/releases/download/v.5.6.1/so-5.6.1.tar.gz'
 
   e.map_dir 'dev/so_5' => 'dev'
 end
 
 MxxRu::arch_externals :asio do |e|
-  e.url 'https://github.com/chriskohlhoff/asio/archive/asio-1-12-2.tar.gz'
+  e.url 'https://github.com/chriskohlhoff/asio/archive/asio-1-14-0.tar.gz'
 
   e.map_dir 'asio/include' => 'dev/asio'
 end
 
 MxxRu::arch_externals :asio_mxxru do |e|
-  e.url 'https://bitbucket.org/sobjectizerteam/asio_mxxru-1.1/get/1.1.1.tar.bz2'
+  e.url 'https://github.com/Stiffstream/asio_mxxru/archive/1.1.2.tar.gz'
 
   e.map_dir 'dev/asio_mxxru' => 'dev'
 end
 
 MxxRu::arch_externals :doctest do |e|
-  e.url 'https://github.com/onqtam/doctest/archive/2.3.3.tar.gz'
+  e.url 'https://github.com/onqtam/doctest/archive/2.3.4.tar.gz'
 
   e.map_file 'doctest/doctest.h' => 'dev/doctest/*'
 end
