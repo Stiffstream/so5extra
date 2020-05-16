57db6abaa260546d1058e011526145dfa057128d
Yauheni Akhotnikau <eao197@gmail.com>
2020-05-16 11:49:59 +0300

    Refactoring of project files for asio_one_thread examples.

--- dev/sample/so_5_extra/disp/asio_one_thread/resolve_n/prj_s.rb
+++ dev/sample/so_5_extra/disp/asio_one_thread/resolve_n/prj_s.rb
@@ -4,12 +4,9 @@ MxxRu::Cpp::exe_target {
 
 	required_prj 'so_5/prj_s.rb'
 	required_prj 'asio_mxxru/prj.rb'
+	required_prj 'test/so_5_extra/env_infrastructures/asio/platform_specific_libs.rb'
+
 	target 'sample.so_5_extra.disp.asio_one_thread.resolve_n_s'
 
 	cpp_source 'main.cpp'
-
-	if 'mswin' == toolset.tag( 'target_os' )
-		lib 'wsock32' 
-		lib 'ws2_32' 
-	end
 }
