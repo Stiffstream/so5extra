ca9e6034ae2ee035fe8720b76d0862f3b95a6bb4
Yauheni Akhotnikau <eao197@gmail.com>
2023-03-24 14:37:47 +0300

    Modernization of broadcasting mbox.

--- dev/so_5_extra/mboxes/broadcast.hpp
+++ dev/so_5_extra/mboxes/broadcast.hpp
@@ -126,8 +126,7 @@ public :
 	void
 	subscribe_event_handler(
 		const std::type_index & /*type_index*/,
-		const message_limit::control_block_t * /*limit*/,
-		agent_t & /*subscriber*/ ) override
+		abstract_message_sink_t & /*subscriber*/ ) override
 		{
 			SO_5_THROW_EXCEPTION( rc_not_implemented,
 					"subscribe_event_handler can't be used for broadcast mbox" );
@@ -136,7 +135,7 @@ public :
 	void
 	unsubscribe_event_handlers(
 		const std::type_index & /*type_index*/,
-		agent_t & /*subscriber*/ ) override
+		abstract_message_sink_t & /*subscriber*/ ) override
 		{
 			SO_5_THROW_EXCEPTION( rc_not_implemented,
 					"unsubscribe_event_handler can't be used for broadcast mbox" );
@@ -159,9 +158,10 @@ public :
 
 	void
 	do_deliver_message(
+		message_delivery_mode_t delivery_mode,
 		const std::type_index & msg_type,
 		const message_ref_t & message,
-		unsigned int overlimit_reaction_deep ) override
+		unsigned int redirection_deep ) override
 		{
 			if( message_mutability_t::mutable_message == message_mutability(message) )
 				SO_5_THROW_EXCEPTION(
@@ -169,14 +169,14 @@ public :
 						"a mutable message can't be sent via broadcast mbox" );
 
 			for( auto & m : m_destinations )
-				m->do_deliver_message( msg_type, message, overlimit_reaction_deep );
+				m->do_deliver_message( delivery_mode, msg_type, message, redirection_deep );
 		}
 
 	void
 	set_delivery_filter(
 		const std::type_index & /*msg_type*/,
 		const delivery_filter_t & /*filter*/,
-		agent_t & /*subscriber*/ ) override
+		abstract_message_sink_t & /*subscriber*/ ) override
 		{
 			SO_5_THROW_EXCEPTION( rc_not_implemented,
 					"set_delivery_filter can't be used for broadcast mbox" );
@@ -185,7 +185,7 @@ public :
 	void
 	drop_delivery_filter(
 		const std::type_index & /*msg_type*/,
-		agent_t & /*subscriber*/ ) noexcept override
+		abstract_message_sink_t & /*subscriber*/ ) noexcept override
 		{}
 
 	environment_t &
