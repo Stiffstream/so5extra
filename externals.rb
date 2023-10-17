bed01042291aa0ee42377b04088ec57718a588b8
Yauheni Akhotnikau <eao197@gmail.com>
2023-10-17 10:39:01 +0300

    Switch to the official v.5.8.1 of SObjectizer.

--- externals.rb
+++ externals.rb
@@ -1,6 +1,5 @@
 MxxRu::arch_externals :so5 do |e|
-#  e.url 'https://github.com/Stiffstream/sobjectizer/archive/v.5.8.0.tar.gz'
-  e.url 'https://github.com/Stiffstream/sobjectizer/archive/b4fb53bdabe48e9c802652de110d81df79a1673a.tar.gz'
+  e.url 'https://github.com/Stiffstream/sobjectizer/archive/v.5.8.1.tar.gz'
 
   e.map_dir 'dev/so_5' => 'dev'
 end
