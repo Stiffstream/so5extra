01e34846d1b4533b5277fbc7a96d78a3ae43e6e0
Yauheni Akhotnikau <eao197@gmail.com>
2024-03-22 16:15:57 +0300

    Switch to Asio-1-29-0.

--- externals.rb
+++ externals.rb
@@ -5,7 +5,7 @@ MxxRu::arch_externals :so5 do |e|
 end
 
 MxxRu::arch_externals :asio do |e|
-  e.url 'https://github.com/chriskohlhoff/asio/archive/asio-1-28-0.tar.gz'
+  e.url 'https://github.com/chriskohlhoff/asio/archive/asio-1-29-0.tar.gz'
 
   e.map_dir 'asio/include' => 'dev/asio'
 end
