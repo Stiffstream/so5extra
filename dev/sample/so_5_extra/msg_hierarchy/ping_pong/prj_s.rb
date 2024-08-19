1cbc6e66530b3d2ad4f2c138bf58e896e4f74b70
Yauheni Akhotnikau <eao197@gmail.com>
2024-08-19 16:06:04 +0300

    Fix names of new examples.

--- dev/sample/so_5_extra/msg_hierarchy/ping_pong/prj_s.rb
+++ dev/sample/so_5_extra/msg_hierarchy/ping_pong/prj_s.rb
@@ -6,7 +6,7 @@ MxxRu::Cpp::exe_target {
 	required_prj 'asio_mxxru/prj.rb'
 	required_prj 'test/so_5_extra/env_infrastructures/asio/platform_specific_libs.rb'
 
-	target 'sample.so_5_extra.disp.asio_one_thread.ping_pong_s'
+	target 'sample.so_5_extra.msg_hierarchy.ping_pong_s'
 
 	cpp_source 'main.cpp'
 }
