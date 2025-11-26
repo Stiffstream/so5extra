9cea64ce66aecd48cc00f7ea11e39e0ad70aa506
Yauheni Akhotnikau <eao197@gmail.com>
2025-11-26 11:15:17 +0300

    A base for development of 1.6.3.
    MxxRu build system is no more supported.
    MxxRu related files have been removed.
    
    CMake is now the build system for so5extra.
    
    Updates for VC++.
    
    Switch to SO-5.8.5.
    
    Update date in LICENSE file.

--- dev/sample/so_5_extra/async_op/time_unlimited/build_samples.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-require 'mxx_ru/cpp'
-
-MxxRu::Cpp::composite_target {
-	path = 'sample/so_5_extra/async_op/time_unlimited'
-
-	required_prj( "#{path}/simple/prj.rb" )
-	required_prj( "#{path}/simple/prj_s.rb" )
-}
--- dev/test/so_5_extra/disp/asio_thread_pool/custom_thread/prj.ut.rb
+++
@@ -1,9 +0,0 @@
-require 'mxx_ru/binary_unittest'
-
-path = 'test/so_5_extra/disp/asio_thread_pool/custom_thread'
-
-MxxRu::setup_target(
-	MxxRu::BinaryUnittestTarget.new(
-		"#{path}/prj.ut.rb",
-		"#{path}/prj.rb" )
-)
